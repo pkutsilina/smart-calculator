7e16b481e3f0adaa3a9cae230c40916ef69758c1
palina_kutsilina <palina.kutsilina@gmail.com>
2018-10-04 23:39:53 +0300

    Solve calculator problem

--- src/index.js
+++ src/index.js
@@ -1,27 +1,127 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    static priorities() {
+        return {
+            '+': 0,
+            '-': 0,
+            '*': 1,
+            '/': 1,
+            '^': 2
+        };
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    static operators() {
+        return ['-', '+', '*', '/', '^'];
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    static isOperatorLeftAssosiative(operator) {
+        return (operator != '^');
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.tempStack = [];
+        this.resultPolskiStrig = [];
+        this.resultPolskiStrig.push(initialValue);
+    }
+
+    add(number) {
+        this.commonFillPolskiString('+');
+        this.resultPolskiStrig.push(number);
+        return this;
+    }
+
+    subtract(number) {
+        this.commonFillPolskiString('-');
+        this.resultPolskiStrig.push(number);
+        return this;
+    }
+
+    multiply(number) {
+        this.commonFillPolskiString('*');
+        this.resultPolskiStrig.push(number);
+        return this;
+    }
+
+    devide(number) {
+        this.commonFillPolskiString('/');
+        this.resultPolskiStrig.push(number);
+        return this;
+    }
+
+    pow(number) {
+        this.commonFillPolskiString('^');
+        this.resultPolskiStrig.push(number);
+        return this;
+    }
+
+    //Maintain reverse polish string and corresponding stack with current operator
+    commonFillPolskiString(operator) {
+        let isContinue = false;
+        do {
+            let topStackElement = this.tempStack.pop();
+            if (topStackElement != undefined) {
+                if (SmartCalculator.priorities()[topStackElement] > SmartCalculator.priorities()[operator]
+                    || (SmartCalculator.priorities()[topStackElement] == SmartCalculator.priorities()[operator]
+                        && SmartCalculator.isOperatorLeftAssosiative(operator))) {
+                    this.resultPolskiStrig.push(topStackElement);
+                    isContinue = true;
+                }
+                else {
+                    this.tempStack.push(topStackElement);
+                    isContinue = false;
+                }
+            } else {
+                isContinue = false;
+            }
+        }
+        while (isContinue);
+        this.tempStack.push(operator);
+    }
+
+    valueOf() {
+        return this.computePolskiString();
+    }
+
+    //Gets result of reverse polish notation
+    computePolskiString() {
+        let it = this.tempStack.pop();
+        while (it != undefined) {
+            this.resultPolskiStrig.push(it);
+            it = this.tempStack.pop();
+        }
+
+        let stackNumbersForComputing = [];
+        for (let i = 0; i < this.resultPolskiStrig.length; i++) {
+            let currentSymbol = this.resultPolskiStrig[i];
+            if (SmartCalculator.operators().includes(currentSymbol)) {
+                let last = stackNumbersForComputing.pop();
+                let preLast = stackNumbersForComputing.pop();
+                switch (currentSymbol) {
+                    case '+':
+                        stackNumbersForComputing.push(preLast + last);
+                        break;
+                    case '-':
+                        stackNumbersForComputing.push(preLast - last);
+                        break;
+                    case '*':
+                        stackNumbersForComputing.push(preLast * last);
+                        break;
+                    case '/':
+                        stackNumbersForComputing.push(preLast / last);
+                        break;
+                    case '^':
+                        stackNumbersForComputing.push(Math.pow(preLast, last));
+                        break;
+                }
+            }
+            else {
+                stackNumbersForComputing.push(currentSymbol);
+            }
+        }
+        let result = stackNumbersForComputing.pop();
+        return result;
+    }
 }
+
 
 module.exports = SmartCalculator;
